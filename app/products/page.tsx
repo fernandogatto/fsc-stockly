bc75359186ae29d8a8a87b66dd43ddac9cafc09d
fernandogatto <fernandogatto17@gmail.com>
2025-03-05 17:28:17 -0300

    feat: add products table

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,29 @@
-const ProductsPage = () => {
+import { PlusIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { DataTable } from "../_components/ui/data-table";
+import { db } from "../_lib/prisma";
+import { productTableColumns } from "./_components/table-columns";
+
+const ProductsPage = async () => {
+  const products = await db.product.findMany({});
+
   return (
-    <div>
-      <h1>Products page</h1>
+    <div className="w-full space-y-8 p-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-gray-500">
+            Gestão de Produtos
+          </span>
+          <h2 className="text-xl font-semibold">Produtos</h2>
+        </div>
+
+        <Button className="gap-2">
+          <PlusIcon size={20} />
+          Novo produto
+        </Button>
+      </div>
+
+      <DataTable columns={productTableColumns} data={products} />
     </div>
   );
 };
